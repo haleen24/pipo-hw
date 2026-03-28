c80aff632c803526a43d0bde72b678405d11433f
ro.a.stepanov <ro.a.stepanov@tbank.ru>
2026-03-28 23:22:24 +0300

    add logging to errors

--- inventory-service/src/main/java/ru/hse/pipo/controller/GlobalControllerAdvice.java
+++ inventory-service/src/main/java/ru/hse/pipo/controller/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package ru.hse.pipo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,16 +8,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 import ru.hse.pipo.exception.InventoryException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(e.getStatusCode())
             .body(ErrorResponse.builder(e, e.getStatusCode(), e.getMessage()).build());
     }
 
     @ExceptionHandler(InventoryException.class)
     public ResponseEntity<ErrorResponse> handleOtherExceptions(InventoryException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(e.getCode().getHttpStatus())
             .body(ErrorResponse.builder(e, e.getCode().getHttpStatus(), e.getDetails()).build());
     }
